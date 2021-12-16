5b21c18e2ec7fcd392a26ead8e8abd0df658f13e
vorvit <94832434+vorvit@users.noreply.github.com>
2021-12-16 23:02:38 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (UIElement el in grid.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += ButtonClick;
+                }
+            }
+        }
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            if (str == "AC")
+                textLabel.Text = "";
+            else if (str == "=")
+            {
+                textLabel.Text = textLabel.Text.Replace(",", ".");
+                string value = new DataTable().Compute(textLabel.Text, null).ToString();
+                textLabel.Text = value.Replace(".", ",");
+            }
+            else
+                textLabel.Text += str;
+        }
+    }
+}
